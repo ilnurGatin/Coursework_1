784026679f74c5d8693dfc80dad5ab0b01198fe8
ilnurGatin <reilag@yandex.ru>
2022-10-10 00:56:14 +0500

    CourseWork

---
+++ src/Employee.java
@@ -0,0 +1,48 @@
+public class Employee {
+    private final int id;
+    private final String fullName;
+    private int departmentId;
+    private int salary;
+    public static int count = 0;
+
+    public Employee(String fullName, int departmentId, int salary) {
+        this.id = count++;
+        this.fullName = fullName;
+        this.departmentId = departmentId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName=" + fullName +
+                ", departmentId=" + departmentId +
+                ", salary=" + salary +
+                '}';
+    }
+}
